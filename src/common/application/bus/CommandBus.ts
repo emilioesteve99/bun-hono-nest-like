eeaf612bc82b427855d1d91cebc557bb77df21d4
Emilio Esteve <emespei@icloud.com>
2025-09-29 11:31:59 +0200

    feat: Add handlers cache in query and command buses

--- src/common/application/bus/CommandBus.ts
+++ src/common/application/bus/CommandBus.ts
@@ -7,14 +7,24 @@ import { Command } from '../model/Command';
 
 @injectable()
 export class CommandBus {
+  private readonly handlersCache: Map<string, CommandHandler<unknown, unknown>> = new Map();
+
   public async execute<TCommand extends Command = Command, TResult = unknown>(command: TCommand): Promise<TResult> {
     const commandName: string = command.constructor.name;
+    const cachedHandler: CommandHandler<TCommand, TResult> | undefined = this.handlersCache.get(
+      commandName,
+    ) as CommandHandler<TCommand, TResult>;
+    if (cachedHandler !== undefined) {
+      return cachedHandler.execute(command);
+    }
+
     const commandHandlerName: string | undefined = commandHandlersByCommandType.get(commandName);
     if (commandHandlerName === undefined) {
       throw new Error(`CommandHandler not found for command: ${commandName}`);
     }
 
-    const commandHandler: CommandHandler<TCommand, TResult> = await di.getAsync(commandHandlerName);
+    const commandHandler: CommandHandler<TCommand, TResult> = di.get(commandHandlerName);
+    this.handlersCache.set(commandName, commandHandler);
     return commandHandler.execute(command);
   }
 }
--- src/common/application/bus/QueryBus.ts
+++ src/common/application/bus/QueryBus.ts
@@ -7,14 +7,25 @@ import { QueryHandler } from '../queryHandler/QueryHandler';
 
 @injectable()
 export class QueryBus {
+  private readonly handlersCache: Map<string, QueryHandler<unknown, unknown>> = new Map();
+
   public async execute<TQuery extends Query = Query, TResult = unknown>(query: TQuery): Promise<TResult> {
     const queryName: string = query.constructor.name;
+    const cachedHandler: QueryHandler<TQuery, TResult> | undefined = this.handlersCache.get(queryName) as QueryHandler<
+      TQuery,
+      TResult
+    >;
+    if (cachedHandler !== undefined) {
+      return cachedHandler.execute(query);
+    }
+
     const queryHandlerName: string | undefined = queryHandlersByCommandType.get(queryName);
     if (queryHandlerName === undefined) {
       throw new Error(`QueryHandler not found for query: ${queryName}`);
     }
 
     const queryHandler: QueryHandler<TQuery, TResult> = di.get(queryHandlerName);
+    this.handlersCache.set(queryName, queryHandler);
     return queryHandler.execute(query);
   }
 }
